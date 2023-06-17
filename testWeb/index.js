72008be10cc985ad8a37bb27235c6b4dc4344b67
Colson <colsoncalifornication@gmail.com>
2023-06-17 18:03:08 +0545

    add testWeb to test web app

---
+++ testWeb/index.js
@@ -0,0 +1,16 @@
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { value } = document.querySelector('input');
+
+  const header = document.querySelector('h1');
+  if (value.includes('@')) {
+    header.classList.remove('netflix-red');
+    header.classList.add('google-blue');
+    header.innerHTML = 'AWESOME 🔥';
+  } else {
+    header.classList.remove('google-blue');
+    header.classList.add('netflix-red');
+    header.innerHTML = 'Invalid email 😂';
+  }
+});
