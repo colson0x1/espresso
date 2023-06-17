b623b59f44ba7b82643c1b8237e55a8b396c8eb9
Colson <colsoncalifornication@gmail.com>
2023-06-17 16:19:49 +0545

    restrict certain directories

--- runner.js
+++ runner.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const importChalk = async () => {
+  try {
+    const chalk = await import('chalk');
+    return chalk.default;
+  } catch (error) {
+    console.error('Failed to import chalk:', error);
+    process.exit(1);
+  }
+};
+
+const forbiddenDirs = ['node_modules'];
 class Runner {
   constructor() {
     this.testFiles = [];
@@ -7,7 +18,53 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
-      require(file.name);
+      const chalk = await importChalk();
+      const testSuccess = chalk.hex('#1DB954');
+      const testFailed = chalk.hex('#E50914');
+
+      const espresso = chalk.hex('#FFA500');
+
+      console.log(
+        `${chalk.bold(
+          `${chalk.grey('Running')} ${espresso('ESPRESSO')} 🔥 ${chalk.grey(
+            'on'
+          )} ${chalk.grey(file.shortName)}`
+        )}`
+      );
+
+      const beforeEaches = [];
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+
+      global.it = (desc, fn) => {
+        beforeEaches.forEach((func) => func());
+        try {
+          fn();
+          console.log(
+            testSuccess(`\t(✔) ${chalk.bold('Test Passed')} - ${desc}`)
+          );
+        } catch (err) {
+          const message = err.message.replace(/\n/g, '\n\t\t');
+          console.log(
+            testFailed(`\t(✘) ${chalk.bold('Test Failed')} - ${desc}`)
+          );
+          console.log(`\t${message}`);
+        }
+      };
+
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log(
+          testFailed(
+            `(✘) ${chalk.bold('\tTest Failed')} - Error Loading File ${
+              file.name
+            }`
+          )
+        );
+        console.log(testFailed(err));
+      }
     }
   }
 
@@ -19,8 +76,8 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes('.test.js')) {
-        this.testFiles.push({ name: filepath });
-      } else if (stats.isDirectory()) {
+        this.testFiles.push({ name: filepath, shortName: file });
+      } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
         const childFiles = await fs.promises.readdir(filepath);
 
         files.push(...childFiles.map((f) => path.join(file, f)));
