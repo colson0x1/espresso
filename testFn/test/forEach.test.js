1c584a67fee5979b2686acfc8b3df1d1e2e39a5d
Colson <colsoncalifornication@gmail.com>
2023-06-17 11:20:49 +0545

    implement 'beforeEach' and 'it'

--- testFn/test/forEach.test.js
+++ testFn/test/forEach.test.js
@@ -1,13 +1,32 @@
 const assert = require('assert');
 const { forEach } = require('../index');
 
-it('should sum an array', () => {
-  const numbers = [2, 4, 6, 8, 4];
+let numbers;
+beforeEach(() => {
+  // setup phase
+  numbers = [2, 4, 6, 8, 4];
+});
 
+it('should sum an array', () => {
+  // exercise phase
   let total = 0;
   forEach(numbers, (value) => {
     total += value;
   });
 
+  // verify phase
   assert.strictEqual(total, 24);
+
+  // teardown phase
+  numbers.push(5);
+  numbers.push(5);
+  numbers.push(5);
+  numbers.push(5);
+  numbers.push(5);
+  numbers.push(5);
+  numbers.push(5);
+});
+
+it('beforeEach is ran each time', () => {
+  assert.strictEqual(numbers.length, 5);
 });
--- testFn/test/map.test.js
+++ testFn/test/map.test.js
@@ -1,10 +1,31 @@
 const assert = require('assert');
-const map = require('../index');
+const { map } = require('../index');
 
-it('should return thrice the given array', () => {
-  const result = map([2, 4, 6, 8], (value) => {
+let numbers;
+beforeEach(() => {
+  // setup phase
+  numbers = [2, 4, 6, 8];
+});
+
+it('should return thrice the array', () => {
+  // exercise phase
+  const result = map(numbers, (value) => {
     return value * 3;
   });
 
+  // verify phase
   assert.deepStrictEqual(result, [6, 12, 18, 24]);
+
+  // teardown phase
+  numbers.push(4);
+  numbers.push(4);
+  numbers.push(4);
+  numbers.push(4);
+  numbers.push(4);
+  numbers.push(4);
+  numbers.push(4);
+});
+
+it('beforeEach is ran each time', () => {
+  assert.strictEqual(numbers.length, 4);
 });
